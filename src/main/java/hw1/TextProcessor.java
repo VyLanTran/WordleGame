a7efba2cae8201a4e65cd58373e4835e9fab1ca2
Vy Tran <vlt002@bucknell.edu>
2022-10-10 17:29:05 -0400

    version 3

--- src/main/java/hw1/TextProcessor.java
+++ src/main/java/hw1/TextProcessor.java
@@ -31,20 +31,33 @@ public class TextProcessor {
     private final String DICTIONARY_URL = "https://www.gutenberg.org/cache/epub/29765/pg29765.txt";
     private URL url;
     private TreeMap<String, Integer> wordMap;
-    private ArrayList<String> masterDictionary;
+//    private ArrayList<String> masterDictionary;
+//    private ArrayList<String> fullMasterDictionary;
+    private Set<String> masterDictionary;
     private int totalWords;
     private int totalGoodWordsDiscarded;
     private int totalGoodWords;
     private int totalUniqueWords;
 
+    private ArrayList<String> goodWords;
+    private ArrayList<String> invalidWords;
+    private ArrayList<String> validButWrongLength;
+    private ArrayList<String> otherErrors;
+
     public TextProcessor(URL url) {
         this.url = url;
-        this.masterDictionary = new ArrayList<>();
+        this.masterDictionary = new TreeSet<>();
         this.wordMap = new TreeMap<>();
         this.totalWords = 0;
         this.totalGoodWordsDiscarded = 0;
         this.totalGoodWords = 0;
         this.totalUniqueWords = 0;
+
+//        this.fullMasterDictionary = new ArrayList<>();
+        this.goodWords = new ArrayList<>();
+        this.invalidWords = new ArrayList<>();
+        this.validButWrongLength = new ArrayList<>();
+        this.otherErrors = new ArrayList<>();
     }
 
     private void generateMasterDictionary() throws IOException {
@@ -54,13 +67,29 @@ public class TextProcessor {
             String word = scnr.next();
             Pattern p = Pattern.compile("[A-Z]+");
             Matcher m = p.matcher(word);
-            if (word.length() == 5 && m.matches() && !masterDictionary.contains(word.toLowerCase())) {
+//            if (m.matches() && !fullMasterDictionary.contains(word.toLowerCase())) {
+//                fullMasterDictionary.add(word.toLowerCase());
+//            }
+//            if (word.length() == 5 && m.matches() && !masterDictionary.contains(word.toLowerCase())) {
+//                masterDictionary.add(word.toLowerCase());
+//            }
+            if (word.length() == 5 && m.matches()) {
                 masterDictionary.add(word.toLowerCase());
             }
         }
+//        for (String w : fullMasterDictionary) {
+//            if (w.length() == 5) {
+//                masterDictionary.add(w);
+//            }
+//        }
+//        System.out.println(fullMasterDictionary.size());
+//        for (String w : fullMasterDictionary) {
+//            System.out.println(w);
+//        }
     }
 
     private void processTextAtURL(URL url) throws IOException {
+        generateMasterDictionary();
         Scanner scnr = new Scanner(url.openStream());
         while (scnr.hasNext()) {
             totalWords++;
@@ -73,12 +102,17 @@ public class TextProcessor {
                     word = m.group(2);
                     if (isWordValid(word) && !wordMap.containsKey(word)) {
                         wordMap.put(word, 1);
+                        goodWords.add(word);
                     }
                     else if (isWordValid(word) && wordMap.containsKey(word)) {
                         wordMap.put(word, wordMap.get(word) + 1);
+                        goodWords.add(word);
                     }
+                    else {validButWrongLength.add(word);}
                 }
+                else {otherErrors.add(word);}
             }
+            else {invalidWords.add(word);}
         }
     }
 
@@ -87,11 +121,51 @@ public class TextProcessor {
     }
 
     public void printReport() {
+        totalGoodWords = goodWords.size();
+        totalGoodWordsDiscarded = totalWords - totalGoodWords;
+        totalUniqueWords = wordMap.size();
 
+        System.out.println("Total number of words processed: " + totalWords);
+        System.out.println("Total good words by wrong length: " + validButWrongLength.size());
+        System.out.println("Total number of words kept: " + totalGoodWords);
+        System.out.println("Number of unique words: " + totalUniqueWords);
+        System.out.println("Top 20 most frequently occurring words");
+        List<Map.Entry<String, Integer>> frequency = sortByReverseFrequency(wordMap);
+        for (Map.Entry pair : frequency) {
+            System.out.println(pair.getKey() + ": " + pair.getValue());
+        }
+
+
+//        for (String w : invalidWords) {
+//            System.out.println(w);
+//        }
+        System.out.println("Invalid: " + invalidWords.size());
+
+//        for (String w : validButWrongLength) {
+//            System.out.println(w);
+//        }
+        System.out.println("Valid but wrong length: " + validButWrongLength.size());
+//
+//        for (String w : otherErrors) {
+//            System.out.println(w);
+//        }
+        System.out.println("Other: " + otherErrors.size());
+
+//        for (String w : goodWords) {
+//            System.out.println(w);
+//        }
+        System.out.println("Valid: " + goodWords.size());
+
+        System.out.println("Unique words: " + wordMap.size());
     }
 
-    public Set<String> getSetOfWords() {
-        return null;
+    public Set<String> getSetOfWords() throws IOException {
+        processTextAtURL(url);
+        TreeSet<String> setOfGoodWords = new TreeSet<>();
+        for (String word : wordMap.keySet()) {
+            setOfGoodWords.add(word);
+        }
+        return setOfGoodWords;
     }
 
     public void writeListOfWords(String fileName) {
@@ -111,18 +185,17 @@ public class TextProcessor {
     }
 
     public static void main(String[] args) throws IOException {
-        TextProcessor t = new TextProcessor(new URL("https://www.bucknell.edu/"));
-        t.generateMasterDictionary();
+        TextProcessor t = new TextProcessor(new URL("https://www.gutenberg.org/files/1342/1342-0.txt"));
+//        t.generateMasterDictionary();
 //        for (String word : t.masterDictionary) {
 //            System.out.println(word);
 //        }
 
-        t.processTextAtURL(new URL("https://www.gutenberg.org/files/1342/1342-0.txt"));
-//        System.out.println(t.totalWords);
-        System.out.println("Unique words: " + t.wordMap.size());
-        List<Map.Entry<String, Integer>> frequency = t.sortByReverseFrequency(t.wordMap);
-        for (Map.Entry pair : frequency) {
-            System.out.println(pair.getKey() + ": " + pair.getValue());
-        }
+//        t.printReport();
+        Set<String> s = t.getSetOfWords();
+        System.out.println(s.size());
+//        for (String w : s) {
+//            System.out.println(w);
+//        }
     }
 }
--- src/main/java/hw1/WordDictionary.java
+++ src/main/java/hw1/WordDictionary.java
@@ -19,5 +19,52 @@
 
 package hw1;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class WordDictionary {
+    public String[] LIST_OF_TEXT_URLS;
+    private Set<String> wordSet;
+    private TextProcessor textProcessor;
+
+    public WordDictionary(String fileName) {
+        this.LIST_OF_TEXT_URLS = new String[]{"https://www.gutenberg.org/files/1342/1342-0.txt",
+                                              "https://www.gutenberg.org/files/1661/1661-0.txt",
+                                              "https://www.gutenberg.org/cache/epub/67583/pg67583.txt",
+                                              "https://www.gutenberg.org/files/76/76-0.txt",
+                                              "https://www.gutenberg.org/cache/epub/16643/pg16643.txt",
+                                              "https://www.gutenberg.org/cache/epub/67584/pg67584.txt"};
+        this.wordSet = new TreeSet<>();
+        this.textProcessor = null;
+    }
+
+    private void generateNewWordSet() throws IOException {
+        for (String urlString : LIST_OF_TEXT_URLS) {
+            textProcessor = new TextProcessor(new URL(urlString));
+            wordSet.addAll(textProcessor.getSetOfWords());
+        }
+    }
+
+//    public void addWords(List<String> wordList) {
+//
+//    }
+
+    public boolean isWordInSet(String word) {
+        return false;
+    }
+
+    public String getRandomWord() {
+        return "";
+    }
+
+    public static void main(String[] args) throws IOException {
+        WordDictionary dict = new WordDictionary("https://www.gutenberg.org/files/1342/1342-0.txt");
+        dict.generateNewWordSet();
+        for (String word : dict.wordSet) {
+            System.out.println(word);
+        }
+    }
 }
